927da1e35f9bfeaf798e6baedde414a75a529e7e
Khusbu Mishra <Khusbu@users.noreply.github.com>
2017-01-22 02:57:36 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if (( $# == 0 )); then
+  echo "Give filename of edgelist as a command line argument to the script"
+  exit
+fi
+
+filename=$1
+r=$2
+gcc LINCOM_ins.c -o output
+
+echo r = $r
+./output $filename $r
+
+if (($? != 0)); then
+  echo "seg fault in LINCOM, exiting out"
+  exit
+fi
+if [ ! -f Membership_$filename ]; then
+  echo Only one community
+  exit
+fi
+./convert -i $filename -o graph.bin -r labels
+./louvain graph.bin -p Membership_$filename
+
+rm graph.bin
+rm Membership_$filename
+rm labels
